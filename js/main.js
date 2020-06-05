2424b6dd819b6ca235f3419187a2ec366e31e2e7
irene macapinlac <irenekaymac@gmail.com>
2020-06-05 23:38:51 +1000

    coded game logic

--- js/main.js
+++ js/main.js
@@ -12,9 +12,35 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
+function getComputerChoice() {
+  const choices = ['rock', 'paper', 'scissors'];
+  const randomNumber = Math.floor(Math.random() * 3); //Math.floor rounds the number to the nearest decimal number and the '* 3' gives us the max number to be random with.
+  return choices[randomNumber];
+}
+// console.log(getComputerChoice());
+
 // A function that compares the User's hand to the PC's hand
 function game(userChoice) {
-  console.log("You chose " + userChoice);
+  const computerChoice = getComputerChoice();
+  // console.log('PC: ' + computerChoice + ' VS ' + 'User: ' + userChoice);
+  switch (userChoice + computerChoice) {
+    case "rockpaper":
+    case "paperrock":
+    case "scissorspaper":
+      console.log("User Wins!");
+      break;
+    case "rockpaper":
+    case "paperscissors":
+    case "scissorsrock":
+      console.log("PC wins!");
+      break;
+    case "rockrock":
+    case "paperpaper":
+    case "scissorsscissors":
+      console.log("It's a draw!");
+      break;
+
+  }
 }
 
 function main() {
