7c33f40a5468ade7e8c14524a0d83f85b3795f3b
irene macapinlac <irenekaymac@gmail.com>
2020-06-05 22:31:38 +1000

    finished css and started variables on JS

---
+++ js/main.js
@@ -0,0 +1,13 @@
+console.log('hellow');
+
+
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("userScore");
+const computerScore_span = document.getElementById(("pcScore"));
+// Use '_' for variable names so you know it's a DOM variable
+const scoreBoard_div = document.querySelector(".scoreboard");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
